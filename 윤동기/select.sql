ee493c00c0de321fd9a6d960dbbf6a43c0c25a0d
ydg010 <97106031+ydg010@users.noreply.github.com>
2025-07-08 10:44:14 +0900

    관리자 요구사항 sql 추가

--- 윤동기/select.sql
+++ 윤동기/select.sql
@@ -36,36 +36,6 @@ INNER JOIN product AS p ON p.product_id = p_i.product_id
 WHERE i.ingr_name = '성분명'
 ;
 
--- [ingredient-005] 성분 등록
--- 필요하다면 update, delete도 만들 수 있을 듯
-INSERT INTO ingredients(
-	ingr_name,
-	`description`,
-	functionality, 
-	`usage`, potential_risks, 
-	safety_rating, 
-	reference_source, 
-	enrolled_id)
-VALUES (
-	'설탕', 
-	'단맛을 내는 당류.', 
-	'감미, 보존', 
-	'식품 전반', 
-	'과다 섭취 시 당뇨 위험', 
-	'EWG 3', 
-	'https://www.ewg.org', 
-	@choyj_id)
-;
-
--- 성분 update
-UPDATE ingredients
-SET ingr_name = '설탕탕',
-	 `description` = '설탕보다 더 달아',
-	 enrolled_id = @yoondk_id
-WHERE ingr_name = '설탕'
-
-
-
 
 -- [product-002] 표시
 -- 제품 명 검색으로 제품명, 브랜드, 카테고리,  이미지 검색
@@ -92,8 +62,66 @@ INNER JOIN ingredients AS i ON p_i.ingr_id = i.ingr_id
 WHERE p.product_name = '제품명'
 ;
 
--- [product--004] 제품 등록
--- 필요하다면 update, delete도 만들 수 있을 듯
+-- [product-003] 상세 표시
+-- 브랜드 명 검색으로 브랜드 명, 제품 명, 카테고리, 이미지, 포함성분 검색
+SELECT p.brand_name,
+		 p.product_name,
+		 p.category, 
+		 p.img_url, 
+		 i.ingr_name
+FROM products AS p
+INNER JOIN product_ingredients AS p_i ON p.product_id = p_i.product_id
+INNER JOIN ingredients AS i ON p_i.ingr_id = i.ingr_id
+WHERE p.brand_name = '브랜드명'
+;
+
+
+
+
+
+-- 관리자 
+-- 성분 추가
+INSERT INTO ingredients(
+	ingr_name,
+	`description`,
+	functionality, 
+	`usage`, potential_risks, 
+	safety_rating, 
+	reference_source, 
+	enrolled_id,
+	reg_date,
+	update_date)
+VALUES (
+	'설탕', 
+	'단맛을 내는 당류.', 
+	'감미, 보존', 
+	'식품 전반', 
+	'과다 섭취 시 당뇨 위험', 
+	'EWG 3', 
+	'https://www.ewg.org', 
+	@choyj_id,
+	CURRENT_TIMESTAMP,
+	CURRENT_TIMESTAMP)
+;
+
+-- 성분 삭제
+DELETE 
+FROM ingredients
+WHERE ingr_id = 1
+;
+
+
+-- 성분 업데이트
+UPDATE ingredients
+SET ingr_name = '설탕탕',
+	 `description` = '설탕보다 더 달아',
+	 enrolled_id = @yoondk_id
+	 update_date = CURRENT_TIMESTAMP
+WHERE ingr_id = 1
+;
+
+
+-- 제품 추가
 INSERT INTO products (
 	product_name, 
 	brand_name, 
@@ -108,6 +136,82 @@ VALUES (
 	'Beverage', 
 	'https://pepsi.com/lime.jpg', 
 	@choyj_id, 
-	NOW(), 
-	NOW())
+	CURRENT_TIMESTAMP,
+	CURRENT_TIMESTAMP)
+;
+
+-- 제품 삭제
+DELETE 
+FROM products
+WHERE product_id = 1
+;
+
+-- 제품 업데이트
+UPDATE products
+SET product_name = '펩시 진짜 라임',
+	 brand_name = 'Pepssssssi'
+	 enrolled_id = @yoondk_id
+	 update_date = CURRENT_TIMESTAMP
+WHERE ingr_id = 1
+;
+
+
+-- 질병 추가
+INSERT INTO diseases (
+	disease_name, 
+	disease_info, 
+	disease_effect, 
+	enrolled_id,
+	reg_date,
+	update_date) 
+VALUES (
+	'아토피 피부염', 
+	'만성 염증성 피부질환으로 피부 장벽 손상과 가려움, 염증이 동반된다.', 
+	'가려움, 건조함, 염증', 
+	@chosw_id,
+	CURRENT_TIMESTAMP,
+	CURRENT_TIMESTAMP)
+;
+
+-- 질병 삭제
+DELETE 
+FROM diseases
+WHERE disease_id = 1
+;
+
+-- 질병 업데이트
+UPDATE diseases
+SET disease_name = '아토피'
+	 disease_effect = '엄청 가려움'
+	 enrolled_id = @yoondk_id
+	 update_date = CURRENT_TIMESTAMP
+WHERE disease_id = 1
+;
+
+
+-- 라이프 스타일 추가
+INSERT INTO life_styles (
+	life_style_name, 
+	enrolled_id,
+	reg_date,
+	update_date) 
+VALUES (
+	'비건', 
+	@chosw_id,
+	CURRENT_TIMESTAMP,
+	CURRENT_TIMESTAMP)
+;
+
+-- 라이프 스타일 삭제
+DELETE 
+FROM life_styles
+WHERE life_style_id = 1
+;
+
+-- 라이프 스타일 업데이트
+UPDATE life_styles
+SET life_style_name = '슈퍼비건'
+	 enrolled_id = @yoondk_id
+	 update_date = CURRENT_TIMESTAMP
+WHERE disease_id = 1
 ;
