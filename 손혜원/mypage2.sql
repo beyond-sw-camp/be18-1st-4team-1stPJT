b547629fc5916974559629b0b39b2c02b162a721
sonhyee <165750994+sonhyee@users.noreply.github.com>
2025-07-08 11:47:12 +0900

    Modify mypage2.sql

--- 손혜원/mypage2.sql
+++ 손혜원/mypage2.sql
@@ -17,19 +17,19 @@ WHERE user_id = 'input_id';
 
 -- member-006 사용자는 자신의 등록 정보(알레르기, 건강 목표 등)를 한눈에 확인 가능.
 SELECT u.user_id,
-	   d.disease_name,
-	   l.life_style_name
+       d.disease_name,
+       l.life_style_name
 FROM users u
-	JOIN user_disease u_d ON u.user_id = u_d.user_id 
-	JOIN disease d ON u.diseases_id = d.disease_id
-	JOIN user_life_styles u_l ON u.user_id = u_l.user_id
-	JOIN life_styles l ON u_l.life_style_id = l.life_style_id
+JOIN user_disease u_d ON u.user_id = u_d.user_id 
+JOIN disease d ON u_d.diseases_id = d.disease_id
+JOIN user_life_styles u_l ON u.user_id = u_l.user_id
+JOIN life_styles l ON u_l.life_style_id = l.life_style_id
 WHERE user_id = 'input_id';
 
 -- member-007 사용자는 관심 제품(찜하기)을 확인 가능.
 SELECT f.user_id,
-		 f.type,
-		 p.product_name
+       f.type,	 
+       p.product_name
 FROM user_favorites f
-JOIN products p ON f.item_id = p.product_name
+JOIN products p ON f.item_id = p.product_id
 WHERE f.user_id = 'input_id';
